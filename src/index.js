6e3a9dc59e53b81eda7ce0b96a0d900b32ee3774
Volha Kazlovich <eexmellie@gmail.com>
2019-02-27 17:12:17 +0300

    feat: add bracket sequence validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let givenString = str;
+  const escapedBracketsPairs = bracketsConfig.map(pair => pair.join('').replace(/(\W)/g, '\\$1'));
+	const bracketsRegex = new RegExp(`(${escapedBracketsPairs.join('|')})`, 'g');
+
+	while (givenString.length) {
+		givenString = givenString.replace(bracketsRegex, '');
+		if (givenString.search(bracketsRegex) === -1) break;
+	}
+
+	return givenString.length === 0;
 }
