4fe4e08fe02f29c558f4161da2a5059e86390191
Volha Kazlovich <eexmellie@gmail.com>
2019-03-01 21:36:33 +0300

    refactor: change str.replace to str.length comparison, fixed indents

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
   let givenString = str;
-  const escapedBracketsPairs = bracketsConfig.map(pair => pair.join('').replace(/(\W)/g, '\\$1'));
-	const bracketsRegex = new RegExp(`(${escapedBracketsPairs.join('|')})`, 'g');
+  const escapedBracketsPairs = bracketsConfig.map(
+    pair => pair.join('')
+                .replace(/(\W)/g, '\\$1')
+  );
+  const bracketsRegex = new RegExp(`${escapedBracketsPairs.join('|')}`, 'g');
 
-	while (givenString.length) {
-		givenString = givenString.replace(bracketsRegex, '');
-		if (givenString.search(bracketsRegex) === -1) break;
-	}
+  while (givenString.length) {
+    const reducedString = givenString.replace(bracketsRegex, '');
+    if (reducedString.length === givenString.length) break;
+    givenString = reducedString;
+  }
 
-	return givenString.length === 0;
+  return givenString.length === 0;
 }
